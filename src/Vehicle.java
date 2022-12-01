5f2fbad1bec2f8c218bf64c659ae89eacdb4ee8a
Deniselop <volejkodenis302@gmail.com>
2022-12-01 21:40:59 +0300

    add commit

---
+++ src/Vehicle.java
@@ -0,0 +1,38 @@
+public class Vehicle {
+
+    String type;
+    String model;
+    String color;
+    double price;
+    String[] seats;
+
+    Vehicle(){
+
+    }
+
+    Vehicle(String type, String model, String color, double price, String... seats){
+        this.type = type;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "type='" + type + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+
+        System.out.println(car);
+
+        Vehicle sportCar = new Vehicle("sportcar", "audi", "black", 100.0, "sportSeat", "standarSeat");
+
+    }
+}
