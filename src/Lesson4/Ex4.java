3df5377c867710841c6ef088dee69c6c58fc1592
Deniselop <volejkodenis302@gmail.com>
2022-12-07 18:30:37 +0300

    add commit

--- src/Lesson4/Ex4.java
+++ src/Lesson4/Ex4.java
@@ -3,6 +3,8 @@ package Lesson4;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static jdk.internal.org.jline.utils.Colors.s;
+
 public class Ex4 {
     public static void main(String[] args) {
 
@@ -16,11 +18,18 @@ public class Ex4 {
         n = in.nextInt();
         int[] arr = new int[n];
 
+
         if (n > 5 && n <= 10) {
 
 
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = (int) (Math.random() * 20);
+               if (arr[i] % 2 == 0 && arr[i] != 0){
+                   String s = String.valueOf(arr[i]);
+               }else{
+
+               }
+
 
             }
 
@@ -28,6 +37,11 @@ public class Ex4 {
             System.out.println("Ошибка! \nВведите размер массива от 5 до 10 включительно");
         }
         System.out.println(Arrays.toString(arr));
+        System.out.println(s);
+
+
+
+
 
 
 
