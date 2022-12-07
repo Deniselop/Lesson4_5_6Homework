740b5b19e1964393800683ccfe131c917879d4aa
Deniselop <volejkodenis302@gmail.com>
2022-12-07 14:28:00 +0300

    add commit

---
+++ src/Lesson4/Ex4.java
@@ -0,0 +1,37 @@
+package Lesson4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+        int n = 0;
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Укажите размер массива от 5 до 10 включительно: \n");
+
+        n = in.nextInt();
+        int[] arr = new int[n];
+
+        if (n > 5 && n <= 10) {
+
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (int) (Math.random() * 20);
+
+
+            }
+
+        }else{
+            System.out.println("Ошибка! \nВведите размер массива от 5 до 10 включительно");
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+
+        int[] arr2 = new int[]
+    }
+}
