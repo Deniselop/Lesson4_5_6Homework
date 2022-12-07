17639b74af6ad9a5868d56114ba848714a8221cd
Deniselop <volejkodenis302@gmail.com>
2022-12-07 22:23:27 +0300

    add commit

---
+++ src/Lesson4/Ex5.java
@@ -0,0 +1,31 @@
+package Lesson4;
+
+import java.util.Arrays;
+
+public class Ex5 {
+    public static void main(String[] args) {
+
+        String[] array = new String[5];
+        int n = 0;
+
+
+        array[0] = "Денис";
+        array[1] = "Настя";
+        array[2] = "Коля";
+        array[3] = "Вася";
+        array[4] = "Антон";
+
+        for (int i = 0; i < array.length; i++ ) {
+
+            if (i != 0 && i % 2 !=0) {
+                array[i] = "null";
+            } else {
+
+            }
+
+            System.out.println(Arrays.toString(array));
+
+
+        }
+    }
+}
